1949cb707d3f7596e221c1a079c981993b599641
Carlos Lopez <celb25@gmail.com>
2024-07-05 16:46:18 -0500

    NEW: Include media fixtures and new testing for Card component.

--- test/components/card_component_test.rb
+++ test/components/card_component_test.rb
@@ -1,12 +1,15 @@
 # frozen_string_literal: true
 
-require "test_helper"
+require 'test_helper'
 
 class CardComponentTest < ViewComponent::TestCase
-  def test_component_renders_something_useful
-    # assert_equal(
-    #   %(<span>Hello, components!</span>),
-    #   render_inline(CardComponent.new(message: "Hello, components!")).css("span").to_html
-    # )
+  def test_render_component
+    items = video_games(:first, :second)
+
+    render_inline(CardComponent.with_collection(items))
+
+    assert_selector('h3', count: 2)
+    assert_selector('h3', text: 'Kingdom Hearts 2')
+    assert_selector('h3', text: 'Elden Ring')
   end
 end
--- test/components/preview_card_component_test.rb
+++ test/components/preview_card_component_test.rb
@@ -5,7 +5,9 @@ require 'test_helper'
 class PreviewCardComponentTest < ViewComponent::TestCase
   def test_render_component
     items = [{ 'name' => 'Elden Ring',
+               'summary' => 'It includes elements that are similar to those in other FromSoftware-developed games',
                'cover' => { 'image_id' => '_id_1234' } }]
+
     render_inline(PreviewCardComponent.with_collection(items))
 
     assert_selector('img[alt="Elden Ring Cover Image"]')
